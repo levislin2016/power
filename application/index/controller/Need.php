ab509384a3b73523a35afcecfb8a04cfdad9d7f5
JinLijin <1105608559@qq.com>
2019-05-24 14:25:06 +0800

    工程管理修改

--- application/index/controller/Need.php
+++ application/index/controller/Need.php
@@ -5,6 +5,7 @@ use app\index\service\Need as NeedService;
 use app\index\model\Project as ProjectModel;
 use app\index\model\Need as NeedModel;
 use app\index\model\Goods as GoodsModel;
+// use app\index\model\SupplyGoods as SupplyGoodsModel;
 use app\lib\exception\BaseException;
 use app\index\validate\NeedValidate;
 
@@ -26,7 +27,8 @@ class Need extends Base
     }
 
     public function add(){ 
-        $goods_list = GoodsModel::with(['supply'])->select();
+        //$goods_list = SupplyGoodsModel::with(['supply', 'goods'])->select();
+        $goods_list = GoodsModel::all();
         $this->assign('goods_list', $goods_list);
 
         $id = input('get.id', '');
--- application/index/service/Need.php
+++ application/index/service/Need.php
@@ -8,16 +8,20 @@ class Need{
 
     public function select_list($params){ 
         $list = NeedModel::useGlobalScope(false)->alias('n')
+            //->leftJoin('supply_goods sg','n.goods_id = sg.id')
+            //->leftJoin('goods g','sg.g_id = g.id')
             ->leftJoin('goods g','n.goods_id = g.id')
             ->leftJoin('unit u','g.unit_id = u.id')
-            ->leftJoin('supply s','g.supply_id = s.id')
+            //->leftJoin('supply s','sg.s_id = s.id')
             ->where(function ($query) use($params) {
                 $query->where('n.company_id', session('power_user.company_id'));
+                $query->where('n.project_id', $params['id']);
                 $query->where('g.company_id', session('power_user.company_id'));
                 $query->where('u.company_id', session('power_user.company_id'));
-                $query->where('s.company_id', session('power_user.company_id'));
+                //$query->where('s.company_id', session('power_user.company_id'));
             })
-            ->field('n.id, n.company_id, n.project_id, n.goods_id, n.need, n.buy, n.have, n.note, n.create_time, g.number, g.name, g.unit_id, g.supply_id, g.image, g.price, u.name as unit, s.name as supply_name')
+            ->field('n.id, n.company_id, n.project_id, n.goods_id, n.need, n.note, n.create_time, g.number, g.name, g.unit_id, g.image, u.name as unit')
+            //->field('n.id, n.company_id, n.project_id, n.goods_id, n.need, n.note, n.create_time, g.number, g.name, g.unit_id, s.id as supply_id, g.image, sg.price, u.name as unit, s.name as supply_name')
             ->order('n.create_time', 'desc')
             ->select();
 
