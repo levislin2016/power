f11eb0c7e6d52a9c567b4ee56b1642f0fd20e2e7
houchengyin <hcy021@gmail.com>
2019-06-04 14:06:47 +0800

    excel

--- application/index/service/Excel.php
+++ application/index/service/Excel.php
@@ -1,49 +1,25 @@
 <?php
 namespace app\index\service;
 
-use app\index\model\Unit as UnitModel;
-use app\lib\exception\BaseException;
+class Excel{
+    public static function excel_data($path){
+        $inputFileName = $path;
+        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
+        $reader->setReadDataOnly(true);
 
-class Unit{
-    //合同列表
-    public function selectList($params){
-        $list = UnitModel::where(function ($query) use($params) {
-            if(!empty($params['search'])){ 
-                $query->where('name', 'like', '%'.$params['search'].'%');
+        $spreadsheet = $reader->load($inputFileName);
+        $worksheet = $spreadsheet->getActiveSheet();
+        $highestRow = $worksheet->getHighestRow(); // e.g. 10
+        $highestColumn = $worksheet->getHighestColumn(); // e.g 'F'
+        $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn); // e.g. 5
+
+        for ($row = 2; $row <= $highestRow; ++$row) {
+            for ($col = 1; $col <= $highestColumnIndex; ++$col) {
+                $data[$row][] = $worksheet->getCellByColumnAndRow($col, $row)->getValue();
             }
-        })->field('id, company_id, name, create_time')->order('create_time', 'desc')->paginate(10, false, [
-            'query'     => $params,
-        ]);
-        return $list;
-    }
-
-    public function add_contract($data){ 
-        $data['company_id'] = session('power_user.company_id');
-        $user = UnitModel::create($data);
-        if(!$user){ 
-            throw new BaseException(
-            [
-                'msg' => '添加计量单位错误！',
-                'errorCode' => 30004
-            ]);
         }
-        return [
-            'msg' => '添加计量单位成功',
-        ];
-    }
-
-    public function save_contract($id, $data){
-        $res = UnitModel::where('id', $id)->update($data);
-        if(!$res){ 
-            throw new BaseException(
-            [
-                'msg' => '修改计量单位错误！',
-                'errorCode' => 30005
-            ]);
-        }
-        return [
-            'msg' => '更改计量单位成功',
-        ];
+        $data = array_values($data);
+        return $data;
     }
 
 } 
