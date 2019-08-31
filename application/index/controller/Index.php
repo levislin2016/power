882c3ab7cd84fa7a071c6e584a3f1193afebb950
houchengyin <hcy021@gmail.com>
2019-08-31 20:12:38 +0800

    左侧列表

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -12,7 +12,7 @@ class Index extends Base
 
     public function index(){
         $left_list = RoleMenu::alias('r')
-            ->field('id, name, graphical, child_id, url')
+            ->field('id, name, graphical, child_id, url, description')
             ->LeftJoin('menu m', 'r.menu_id = m.id')
             ->where('r.role_id', session('power_user')['type'])
             ->where('m.parent_id',0)
@@ -22,7 +22,7 @@ class Index extends Base
             $child_list=[];
             $childs = explode(',', $v['child_id']);
             $child_list = Menu::field('id, name, graphical, url, description')->where('id', 'in', $childs)->select();
-            $v['child_list'] = $child_list;
+            $v['child_list'] = $child_list->toArray();
             unset($v['child_id']);
         }
         $this->assign('list', $left_list);
